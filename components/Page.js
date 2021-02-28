d9344e711f95620eaec289a46634da30eb75e971
Sam Mudick <smudick@gmail.com>
2021-02-28 17:31:34 -0600

    nav and general setup

---
+++ components/Page.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import Header from './Header';
+
+export default function Page({ children, cool }) {
+  return (
+    <div>
+      <Header />
+      <h2>I am the page component</h2>
+      <h2>{cool}</h2>
+      {children}
+    </div>
+  );
+}
+
+Page.propTypes = {
+  cool: PropTypes.string,
+  children: PropTypes.any,
+};
